83e17ecd0741f6212af205aea366c9efa21b9275
Spotexx <travesimo@gmail.com>
2022-02-01 11:58:48 -0500

    chat works need refinement

--- assets/javascript/salbotapi.js
+++ assets/javascript/salbotapi.js
@@ -1,8 +1,15 @@
-let prompt =    `Salbot is your tutor. He has knowledge of javascript, html, css and can answer your questions in a clear and concise manner.` +
-                `The following is a conversation between you and Salbot.`;
-let conversationHistory = `Salbot: "Hey, I heard you were struggling with javascript in class today. Did you have any questions?"` +
-                            `You: "Yea sal, I was wondering if you could explain api's again to me?"` +
-                            `Salbot: "`;
+//conversation prompt. This will always be the same
+let prompt = `Salbot is your tutor. He has knowledge of javascript, html, css and can answer your questions in a clear and concise manner.` +
+    `\nThe following is a conversation between you and Salbot. The conversation will follow the following format` +
+    `\nSalbot: "Hello welcome to my tutoring session."` +
+    `\nYou: "Thank you."` +
+    `\nSalbot: "We can start if youre ready"` +
+    `\nYou: "Yes I am ready"`;
+
+let conversationHistory = localStorage.getItem('conversationHistory') || 
+    [`\nSalbot: "Hey, I heard you were struggling with javascript in class today. Did you have any questions?"`];
+
+
 
 let requestChatResponse = (prompt, conversationHistory) => {
     fetch("https://api.ai21.com/studio/v1/j1-large/complete", {
@@ -23,6 +30,34 @@ let requestChatResponse = (prompt, conversationHistory) => {
         .then(response => response.json())
         .then(aiResponse => {
             console.log(aiResponse);
-            $('body').text(aiResponse.completions[0].data.text);
+            $('#chatBox').append(`<div class="salbotChatContent">${aiResponse.completions[0].data.text}</div>`);
+            conversationHistory[conversationHistory.length -1] += ` ${aiResponse.completions[0].data.text}`;
+            
         })
 }
+
+$('#chatBtn').on('click', () => {
+    let chatInput = $('#chatinput > button').val();
+    conversationHistory.push(`\nYou: "${$('#chatInput').val()}"`);
+    $('#chatBox').append(`<div class="userChatContent">${conversationHistory[conversationHistory.length -1]}</div>`);
+    $('#chatInput').val('');
+    conversationHistory.push(`\nSalbot: "`);
+    console.log(conversationHistory);
+    requestChatResponse(prompt, conversationHistory);
+
+
+});
+
+let initSalbot = () => {
+    for(let index in conversationHistory) {
+        // if index char  is s or S
+        if(conversationHistory[index].charAt(1) === 's' || conversationHistory[index].charAt(1) === 'S') {
+         
+            $('#chatBox').append(`<div class="salbotChatContent">${conversationHistory[index]}</div>`);
+        } else {
+            $('#chatBox').append(`<div class="userChatContent">${conversationHistory[index]}</div>`);
+        }
+    }
+}
+initSalbot();
+
