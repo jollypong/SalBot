7e5985ebd8bba416922926f3cdbd48939396d5e6
Alex C <cxz980314@gmail.com>
2022-02-02 14:52:06 -0500

    update search button

--- assets/javascript/wikiAPI.js
+++ assets/javascript/wikiAPI.js
@@ -5,10 +5,9 @@ async function searchUp(textblock) {
     let ids = "";
     let links = [];
     let results = [];
-    // $("#output").dialog( "open" );
-    // $("#myModal").modal();
-    const value = await generateSearchTerm(textblock);
-    console.log(value);
+    const value = textblock;
+    // const value = await generateSearchTerm(textblock);
+    console.log("value: "+value);
 
 
     fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=${value}`)
@@ -32,30 +31,21 @@ async function searchUp(textblock) {
                         links.push(idresult.query.pages[i].fullurl)
                     }
                 }).then(function (g) {
-                    document.getElementsByClassName("modal-body").innerHTML = "";
+                    document.getElementById("output").innerHTML = "";
                     for (let i = 0; i < results.length; i++) {
-                        document.getElementsByClassName("modal-body").innerHTML +=
-                            "<a href=`" + links[i] + "` target='_blank'>" + results[i].title + "</a><br>" + results[i].snippet + "<br>";
-                    }
-                    
+                        document.getElementById("output").innerHTML +=
+                        `<a href=${links[i]} target='_blank'>` + results[i].title + "</a><br>" + results[i].snippet+ "<br>";
+                    } 
+                    $(".modal").attr("aria-hidden","true");
+                    $('.modal-backdrop').remove();  
                 })
         })
 }
 
 
-
-//  $( "#output" ).dialog({ autoOpen: true});
-
-$("#chatBtn").on("click", async function () {
-    await new Promise((resolve) =>
-        setTimeout(resolve, 2000))
-        $("#messages").children().on("click", function () {
-            let textContent = $(this).text().slice(1);
-            searchUp(textContent);
-            
-        })
+$("#searchButton").on("click", function(){
+    let value = $("#input").val();
+    searchUp(value);
 })
 
-// $(document).ready(function(){
-//     $("#myModal").modal();
-//  });
+$( "#output" ).dialog({ autoOpen: false });
