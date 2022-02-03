c5c716ee10a7d8f6338e9002f46856008705a402
Spotexx <travesimo@gmail.com>
2022-02-02 19:50:28 -0500

    cleaned up some of my work in wikiAPI

--- assets/javascript/wikiAPI.js
+++ assets/javascript/wikiAPI.js
@@ -8,26 +8,26 @@ async function searchUp(textblock) {
     const value = textblock;
 
     fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=${value}`)
-        .then(function (response) {
-            return response.json();
-        }).then(function (result) {
+        .then(response => response.json())
+        .then(function (result) {
             for(let i in result.query.search){
                 idsArray.push(result.query.search[i].pageid);
                 snippetsArray.push(result.query.search[i].snippet);
                 idsString += idsArray[i] + "|";
             }
             idsString = idsString.substring(0, idsString.length - 1);
-        }).then(function (a) {
+        })
+        .then(function (a) {
             fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&origin=*&format=json&pageids=${idsString}`)
                 .then(function (idresult) {
                     return idresult.json();
-                }).then(function (idresult) {
-                    return idresult
-                }).then(function (g) {
+                }).then( idresult => idresult )
+                .then(function (g) {
+                    let pages = g.query.pages;
                     document.getElementById("output").innerHTML = "";
                     for (let i = 0; i < idsArray.length; i++) {
                         document.getElementById("output").innerHTML +=
-                            `<a href=${g.query.pages[idsArray[i]].canonicalurl} target='_blank'>` + g.query.pages[idsArray[i]].title + "</a><br>" + snippetsArray[i] + "<br>";
+                            `<a href=${pages[idsArray[i]].canonicalurl} target='_blank'>` + pages[idsArray[i]].title + "</a><br>" + snippetsArray[i] + "<br>";
                             $(".modal").attr("aria-hidden", "true");
                         $('.modal-backdrop').remove();
                     }
