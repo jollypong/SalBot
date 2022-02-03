f14df9b53afd3364392c6b200676fd2ff472eaf8
Spotexx <travesimo@gmail.com>
2022-02-02 19:33:12 -0500

    fixed wikipedia api link order

--- assets/javascript/wikiAPI.js
+++ assets/javascript/wikiAPI.js
@@ -2,48 +2,41 @@ let buttonEl = document.querySelector("#search");
 let inputEl = document.querySelector("#input");
 
 async function searchUp(textblock) {
-    let ids = "";
-    let links = [];
-    let results = [];
+    let idsArray = [];
+    let idsString = "";
+    let snippetsArray = [];
     const value = textblock;
-    // const value = await generateSearchTerm(textblock);
-    console.log("value: "+value);
-
 
     fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=${value}`)
         .then(function (response) {
             return response.json();
         }).then(function (result) {
-            results = result.query.search;
-            for (let i = 0; i < results.length; i++) {
-                if (results[i + 1] != null) {
-                    ids += results[i].pageid + "|";
-                } else {
-                    ids += results[i].pageid;
-                }
+            for(let i in result.query.search){
+                idsArray.push(result.query.search[i].pageid);
+                snippetsArray.push(result.query.search[i].snippet);
+                idsString += idsArray[i] + "|";
             }
+            idsString = idsString.substring(0, idsString.length - 1);
         }).then(function (a) {
-            fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&origin=*&format=json&pageids=${ids}`)
+            fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&origin=*&format=json&pageids=${idsString}`)
                 .then(function (idresult) {
                     return idresult.json();
                 }).then(function (idresult) {
-                    for (i in idresult.query.pages) {
-                        links.push(idresult.query.pages[i].fullurl)
-                    }
+                    return idresult
                 }).then(function (g) {
                     document.getElementById("output").innerHTML = "";
-                    for (let i = 0; i < results.length; i++) {
+                    for (let i = 0; i < idsArray.length; i++) {
                         document.getElementById("output").innerHTML +=
-                        `<a href=${links[i]} target='_blank'>` + results[i].title + "</a><br>" + results[i].snippet+ "<br>";
-                    } 
-                    $(".modal").attr("aria-hidden","true");
-                    $('.modal-backdrop').remove();  
+                            `<a href=${g.query.pages[idsArray[i]].canonicalurl} target='_blank'>` + g.query.pages[idsArray[i]].title + "</a><br>" + snippetsArray[i] + "<br>";
+                            $(".modal").attr("aria-hidden", "true");
+                        $('.modal-backdrop').remove();
+                    }
                 })
         })
 }
 
 
-$("#searchButton").click(function(e) {
-    e.preventDefault();
-    $(".modal").addClass("is-active");  
-});
+$("#searchButton").on("click", function () {
+    let value = $("#input").val();
+    searchUp(value);
+})
